85a2534ec47323fa99e38de89c8a34822ed8a1c5
MKSalad <khsalaad94@gmail.com>
2026-09-22 19:27:45 +0200

    fix public Pages base path

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/wakiil-automation-demo/',
+  base: '/wakiil-company-platform/',
   plugins: [react()],
 });
